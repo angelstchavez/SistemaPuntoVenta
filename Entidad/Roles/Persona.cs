31af62889f1d321fe46429ffe2304f277c06865b
Cgarias <carlogd7@gmail.com>
2022-11-01 17:36:27 -0500

    Se crean las clases iniciales.

---
+++ Entidad/Roles/Persona.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entidad.Roles
+{
+    internal class Persona
+    {
+    }
+}
