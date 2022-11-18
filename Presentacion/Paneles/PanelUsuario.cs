d3c35db2fcae422bbc75070afd86a4db2f2a1333
Cgarias <carlogd7@gmail.com>
2022-11-18 00:11:30 -0500

    Modificación formulario de usuarios.

--- Presentacion/Paneles/PanelUsuario.cs
+++ Presentacion/Paneles/PanelUsuario.cs
@@ -1,4 +1,8 @@
-﻿using System.Windows.Forms;
+﻿using Entidad.Roles;
+using Logica.Logica.Roles;
+using Presentacion.Recursos;
+using System.Collections.Generic;
+using System.Windows.Forms;
 
 namespace Presentacion.Paneles
 {
@@ -16,11 +20,22 @@ namespace Presentacion.Paneles
 
         private void LlenarCombobox()
         {
-            boxEstado.Items.Add(new Recursos.OpcionCombo() { valor = 1, texto = "ACTIVO"});
-            boxEstado.Items.Add(new Recursos.OpcionCombo() { valor = 2, texto = "INACTIVO" });
+            boxEstado.Items.Add(new OpcionCombo() { valor = 1, texto = "ACTIVO"});
+            boxEstado.Items.Add(new OpcionCombo() { valor = 2, texto = "INACTIVO" });
             boxEstado.DisplayMember = "Texto";
             boxEstado.ValueMember = "valor";
             boxEstado.SelectedIndex = 0;
+
+            LogicaRol logicaRol = new LogicaRol();
+            List<Rol> roles = logicaRol.Listar();
+
+            foreach (var item in roles)
+            {
+                boxRol.Items.Add(new OpcionCombo() { valor = 0, texto = item.Descripcion} );
+            }
+            boxRol.DisplayMember = "Texto";
+            boxRol.ValueMember = "valor";
+            boxRol.SelectedIndex = 0;
         }
     }
-}
+    }
